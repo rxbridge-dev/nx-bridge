c534eb0d68c54aea91c9c9356ec1e5cba5a1d4a2
A.M.Ransara Dewnath <ransaramax4@gmail.com>
2026-01-24 10:59:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,18 @@
-module.exports = require(process.env.USER_CONFIG || '../user/config')
+// core/config.js
+// This file always loads the user's config from the USER repo
+
+const path = require('path');
+
+const userConfigPath = process.env.USER_CONFIG
+  ? process.env.USER_CONFIG
+  : path.join(__dirname, '../config');
+
+try {
+  module.exports = require(userConfigPath);
+  console.log('✅ User config loaded from:', userConfigPath);
+} catch (err) {
+  console.error('❌ Failed to load user config!');
+  console.error('Expected path:', userConfigPath);
+  console.error(err);
+  process.exit(1);
+}
